d6c53577591eb31dc206bf3f5c6fa6d48f54b4c7
jreque21 <johnnyreque@gmail.com>
2025-02-16 19:35:37 -0500

    Limpieza

--- Py_JR_Backend/src/main/java/com/jreque/examen1/repository/PagoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jreque.examen1.repository;
-
-import com.jreque.examen1.entity.PagoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagoRepository extends JpaRepository<PagoEntity, Long> {
-}
