a39119a3561e262a254eeb04f5e05fc0097339ad
jreque21 <johnnyreque@gmail.com>
2025-02-16 19:30:00 -0500

    Limpieza

--- TareaJohnnyReque/01_apisExt_restTemplate/ms-registros/src/main/java/com/codigo/ms_registros/repository/PersonaJuridicaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codigo.ms_registros.repository;
-
-import com.codigo.ms_registros.entity.PersonaJuridicaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaJuridicaRepository extends JpaRepository<PersonaJuridicaEntity, Long> {
-}
--- examen1/src/main/java/com/jreque/examen1/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jreque.examen1.repository;
-
-import com.jreque.examen1.entity.PedidoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<PedidoEntity, Long> {
-}
